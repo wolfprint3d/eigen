469e645cb2e9decaccc31ea4887fde65b44c59a3
Jorma <RedFox20@users.noreply.github.com>
2017-10-08 16:34:48 +0300

    Update Functors.h

--- Eigen/src/Core/Functors.h
+++ Eigen/src/Core/Functors.h
@@ -969,7 +969,7 @@ template<typename T>
 struct functor_traits<std::not_equal_to<T> >
 { enum { Cost = 1, PacketAccess = false }; };
 
-#if(__cplusplus < 201103L)
+#if (__cplusplus < 201103L) && (_MSC_VER <= 1900)
 // std::binder* are deprecated since c++11 and will be removed in c++17
 template<typename T>
 struct functor_traits<std::binder2nd<T> >
